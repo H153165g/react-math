dc6ae0cdca5c4b07a07f176b981d6aeb9902f76f
H153165g <chrn22018@g.nihon-u.ac.jp>
2024-05-20 23:36:07 +0900

    提出

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,96 @@
-const App = () => {
-    const width = 400;
-    const height = 400;
+import React from "react";
+import * as d3 from "d3";
+class Chart extends React.Component {
 
-    const rect = [
-        { label: "A", x: 100, y: 100, width: 250, height: 30, fill: "orange" },
-        { label: "B", x: 100, y: 200, width: 200, height: 30, fill: "purple" },
-        { label: "C", x: 100, y: 300, width: 100, height: 30, fill: "pink" },
+    render() {
+        const { data } = this.props
 
-    ];
-    
-    return (
-        <svg width={width} height={height}>
-            <g>
-                <line x1="100" y1="0" x2="100" y2="400" stroke="black" />
-                {rect.map(({ label, x, y, width, height, fill }, i) => {
-                    console.log(label)
-                    return (
-                <g key={i}>
+        const width = 1000
+        const height = 300
+        const color = d3.scaleOrdinal(d3.schemeCategory10)
+        const xmax = d3.max(data["series"].map((_, i) => {
+            return d3.max(data["series"][i]["values"])
+        }))
+        
+        const A = Array.from({ length: Math.floor(xmax / 100) + 1 + 1 }).map((_, i) => i * 100)
 
-                    <rect
+        const xScale = d3.scaleLinear()
+            .domain([0,  (Math.floor(xmax / 100) + 1) * 100])
+            .range([0, width-50-25])
+            .nice()
+        const yw = (height - 50) / data["labels"].length
+        const yh = 1 + data["series"].length * 3
+        const yhscale = d3.scaleLinear()
+            .domain([0, yh])
+            .range([0, yw])
+            .nice()
+        return (<svg width={width} height={height}>
+            {A.map((item) => {
+                return (
+                    <>
+                        <line x1={xScale(item)+50} y1="0" x2={xScale(item)+50} y2={height - 40} stroke="black" />
+                        <text
+                            x={50+xScale(item)}
+                            y={height - 20}
+                            fontSize="20"
+                            textAnchor="middle"
+                            fill="black"
+                            strokeWidth="3">{item}</text>
+                    </>
+                )
+            })}
+            <line x1="50" y1={yhscale(1 + data["series"].length * 3) * data["labels"].length} x2={width-25} y2={yhscale(1 + data["series"].length * 3) * data["labels"].length} stroke="black" />
+            {
+                data["labels"].map((item, i) => {
+                    return (<>
+                            <line x1="40" y1={yw * i + yhscale(1 + data["series"].length * 3) / 2} x2="50" y2={yw * i + yhscale(1 + data["series"].length * 3) / 2} stroke="black" />
+                            <text
+                                x="25"
+                                y={yw * i + yhscale(1 + data["series"].length * 3) / 2+10}
+                                fontSize="20"
+                                textAnchor="middle"
+                                fill="black"
+                                strokeWidth="3">{item}</text>
+                            {data["series"].map((_, j) => {
+                                return (
+                                    <rect x="50" y={yw * i + yhscale(1 + j * 3)} width={xScale(data["series"][j]["values"][i])} height={yhscale(2)} fill={color(j)} />
+                                )
+                            })}
+                            </>
 
-                        x={x}
-                        y={y}
-                        width={width}
-                        height={height}
-                        fill={fill}
-                    />
-                    
 
-                    <text
 
-                        x={x - 20}
-                        y={y + 15}
-                        textAnchor="middle"
-                        dominantBaseline="central">
-                        {label}
-                    </text>
-                    <line x1={x-10} y1={y+15} x2={x} y2={y+15} stroke="black"/>
-                </g>
                     )
+                })
 
-                })}
+            }
 
-            </g>
         </svg>
-    );
-};
+        )
+    }
+}
 
+class App extends React.Component {
+    render() {
+        const data = {
+            labels: ['A', 'B', 'C', 'D'],
+            series: [
+              {
+                name: 'data',
+                values: [123, 456, 789, 1111]
+              },
+              {
+                name: 'another data',
+                values: [234, 567, 891, 1024]
+              },
+              {
+                name: 'and more',
+                values: [567, 678, 789, 890]
+              }
+            ]
+          }
+
+        return <Chart data={data} />
+    }
+}
 export default App;
